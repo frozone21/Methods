72f176336375f70a5ee9ff54401f9c482bbe2034
Bryce Roberts <kendall13@usf.edu>
2020-02-06 17:10:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user their name 
+            Console.WriteLine("Please enter your name");
+
+            //to handle input errors 
+            try
+            {
+                //Read input from user 
+                string name = Console.ReadLine();
+
+                //Calling the method and using the user name 
+                User_Name(name);
+            }
+            catch
+            {
+                //Show this message if there is a wrong input
+                Console.WriteLine("You have entered invalid data");
+            }
+        }
+        //Calling the customers user name 
+        private static string User_Name(string get_name)
+        {
+
+            Console.WriteLine("");
+            //Display "hello" along with the users name 
+            Console.WriteLine("Hello " + get_name + "!");
+
+            //return value
+            return get_name;
+        }
+    }
+}
